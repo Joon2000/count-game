c2f8dcf41449bfe0eab3983c48b49cd695cb6fee
Joon2000 <najoonwon22@gmail.com>
2025-02-21 23:01:29 +0900

    Add game logics

--- src/GameVault.sol
+++ src/GameVault.sol
@@ -1,13 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Vault {
+contract GameVault {
     uint256 public amount;
     address public owner;
     address public withdrawer;
 
+    mapping(address => uint) public attempByPlayer;
+
     event Deposited(address indexed sender, uint256 amount);
     event Withdrawn(address indexed receiver, uint256 amount);
+    event Fail(address indexed player);
+    event Success(address indexed player);
 
     constructor() {
         owner = msg.sender;
@@ -27,11 +31,6 @@ contract Vault {
         withdrawer = selectedAddress;
     }
 
-    function deposit() public payable {
-        require(msg.value > 0, "Deposit amount must be greater than zero");
-        amount += msg.value;
-        emit Deposited(msg.sender, msg.value);
-    }
 
     function withdraw() public onlyWithdrawer {
         require(amount > 0, "No funds available for withdrawal");
@@ -42,5 +41,13 @@ contract Vault {
         require(success, "Transfer failed");
 
         emit Withdrawn(msg.sender, withdrawAmount);
+    }
+
+    function play() public payable {
+        require(attempByPlayer[msg.sender]<5, "Play limit: 5 times");
+        require(msg.value > 0, "Deposit amount must be greater than zero");
+        amount += msg.value;
+        emit Deposited(msg.sender, msg.value);
+        attempByPlayer[msg.sender] += 1;
     }
 }
